a17cf35fe503ee20d1258dd2f0ec0063526f5ffc
David <davidarcila93@gmail.com>
2015-09-07 00:23:56 -0500

    Codeforces

---
+++ Codeforces/src/Bots.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Bots {
+	static long mod = 1000000007;
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int MAX = 2000010;
+		
+		long[] fact = new long[MAX];
+		long[] inv = new long[MAX/2];
+		
+		fact[0] = 1l;
+		for(int i=1; i<fact.length; i++){
+			fact[i] = (fact[i-1]*i)%mod;
+		}
+		for(int i=0; i<inv.length; i++){
+			inv[i] = fastPow(fact[i], (int)(mod-2));
+		}
+//		System.out.println(fact[2*n+2]+" "+inv[n+1]);
+//		System.out.println(inv[20]);
+		long ans = ((((fact[2*n+2]*inv[n+1])%mod) *inv[n+1])+mod-1)%mod;
+		
+		System.out.println( ans);
+	}
+	
+	public static long fastPow(long n, int exp){
+		if(exp == 0)
+			return 1;
+		long ans = fastPow(n, exp/2);
+		ans = (ans*ans)%mod;
+		if(exp%2!=0)
+			ans=(ans*n)%mod;
+		return ans;
+	}
+
+}
---
+++ Codeforces/src/TableCity.java
@@ -0,0 +1,15 @@
+
+public class TableCity {
+
+	public static void main(String[] args)throws Exception {
+		System.out.println("2000");
+		for(int i=1; i<=1000; i++){
+			System.out.print(i+" "+1+" ");
+			System.out.println(i+" "+2);
+		}
+		for(int i=1000; i>0; i--){
+			System.out.print(i+" "+1+" ");
+			System.out.println(i+" "+2);
+		}
+	}
+}
