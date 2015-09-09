b21cf3cd0cebed0318d82ed02a331347dd672686
DavidMauricio <david.arcila@od1.co>
2015-09-09 11:27:16 -0500

    asdf

---
+++ Codeforces/src/MishaAndChangingHandles_501B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+
+public class MishaAndChangingHandles_501B {
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] s;
+		TreeMap<String, String> map = new TreeMap<String, String>();
+		
+		for(int i =0; i<n; i++){
+			s = br.readLine().split(" ");
+		}
+	}
+}
